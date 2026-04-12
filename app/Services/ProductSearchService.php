be37d0b5bf68b3afc01518fe9d0006f5bc0dff0c
bonettrenzo <bonettrenzo@gmail.com>
2026-04-11 23:25:07 -0500

    chore: configuracion elasticsearch para prod

--- app/Services/ProductSearchService.php
+++ app/Services/ProductSearchService.php
@@ -9,8 +9,12 @@ class ProductSearchService implements IProductSearchService
 {
     public function search(string $query)
     {
+        $apiKey = config('services.elasticsearch.api_key');
+        list($id, $key) = explode(':', base64_decode($apiKey));
+
         $client = ClientBuilder::create()
             ->setHosts([config('services.elasticsearch.host')])
+            ->setApiKey($id, $key)
             ->build();
 
         $params = [
